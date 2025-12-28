adeccb330f9e669c352f78d6da4f92df0eb8b7c2
Vangapally Bhanu_Dev <114730455+Bhanu-Vangapally@users.noreply.github.com>
2025-12-28 16:52:48 +0530

    Create 1646-KthMissingPositiveNumber

---
+++ 1646-KthMissingPositiveNumber/1646-KthMissingPositiveNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 12/28/2025, 4:52:44 PM
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int ans[]=new int[arr.length+k];
+        for(int i=1;i<=arr.length+k;i++) ans[i-1]=i;
+        ArrayList<Integer>l=new ArrayList<>();
+        for(int i:arr){
+            l.add(i);
+        }
+        int p=k;
+        for(int i=0;i<arr.length+k;i++){
+            if(!l.contains(ans[i]) && p!=0) p--;
+            if(p==0) return ans[i]; 
+        }
+        return -1;
+    }
+}
