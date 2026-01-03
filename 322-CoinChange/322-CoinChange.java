7104966d32907273710e8a79e22f729ba527c966
Vangapally Bhanu_Dev <114730455+Bhanu-Vangapally@users.noreply.github.com>
2026-01-03 15:41:05 +0530

    File updated at 1/3/2026, 3:41:07 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,20 +1,14 @@
-// Last updated: 12/28/2025, 4:53:43 PM
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int [] ans=new int[amount+1];
-        Arrays.fill(ans,amount+1);
-        ans[0]=0;
-        for(int i=1;i<amount+1;i++){
-            for(int coin:coins){
-                if(i-coin>=0)
-                {
-                    ans[i]=Math.min(1+ans[i-coin], ans[i]);
-                }
-            }
-        }
-        // for(int i: ans)
-
-        return ans[amount] > amount ? -1: ans[amount];
-
-    }
-}
+// Last updated: 1/3/2026, 3:41:07 PM
+1class Solution {
+2    public int coinChange(int[] coins, int amount) {
+3        int dp[]=new int[amount+1];
+4        Arrays.fill(dp,amount+1);
+5        dp[0]=0;
+6        for(int amt=1;amt<=amount;amt++){
+7            for(int c:coins){
+8                if(c<=amt) dp[amt]=Math.min(dp[amt],dp[amt-c]+1);
+9            }
+10        }
+11        return dp[amount]!=amount+1?dp[amount]:-1;
+12    }
+13}
